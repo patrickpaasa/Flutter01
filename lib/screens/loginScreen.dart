2ad501c7d94266e5aa2217b54b99b1372f9d6935
Pating ng AdDU <patrick.paasa@gmail.com>
2022-01-13 11:09:31 +0800

    added constants, assets; use stacks, image, etc.,

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter01/shared/Constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -7,12 +8,13 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: uiColors,
           title: const Text("Multi-platform"),
           elevation: 0,
         ),
         body: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints constraint) {
-            if (constraint.maxWidth > 600) {
+            if (constraint.maxWidth > 800) {
               return Container(
                 width: MediaQuery.of(context).size.width,
                 child: Row(
@@ -22,21 +24,47 @@ class LoginScreen extends StatelessWidget {
                   children: [
                     Container(
                       width: MediaQuery.of(context).size.width * .65,
-                      color: Colors.green[100],
+                      child: Image(image: AssetImage("assets/migpro.jpeg"),
+                        fit: BoxFit.cover,),
                     ),
                     Container(
                       width: MediaQuery.of(context).size.width * .35,
+                      color: uiColors[20],
                       child: loginButtons(),
                     )
                   ],
                 ),
               );
             } else {
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                color: Colors.green[100],
-                child: loginButtons(),
+              return Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width, 
+                    child:  Image(image: AssetImage("assets/migpro.jpeg"),
+                    fit: BoxFit.fitHeight,),
+                  ),
+                  Opacity(
+                    opacity: 0.5,
+                    child: Container(
+                      color: uiColors,
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width, 
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.transparent,
+                    child: loginButtons(),
+                  ),
+                   
+                ],
               );
+              // return Container(
+              //   width: MediaQuery.of(context).size.width,
+              //   color: Colors.green[100],
+              //   child: loginButtons(),
+              // );
             }
           },
         ));
---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor uiColors = MaterialColor(0xFF10228d, <int, Color>{
+  10: Color(0xFFa1affc),
+  20: Color(0xFF152DBB),
+  30: Color(0xFF4267B2),
+});
